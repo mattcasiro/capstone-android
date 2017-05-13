6bb5dd2afe1e0bccbd70fe00afe4ece860532ed4
andrea <andrea.czar@gmail.com>
2017-05-12 23:04:19 -0700

    Add Back button and Bottom sheet.

--- app/src/main/java/com/telmediq/docstorage/activity/FileViewActivity.java
+++ app/src/main/java/com/telmediq/docstorage/activity/FileViewActivity.java
@@ -4,21 +4,43 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.telmediq.docstorage.R;
 import com.telmediq.docstorage.TelmediqActivity;
+import com.telmediq.docstorage.fragment.BottomSheetFileDetailsFragment;
+
+import net.steamcrafted.materialiconlib.MaterialIconView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class FileViewActivity extends TelmediqActivity {
 	@BindView(R.id.fileViewActivity_fileView)
-	ImageView imageView;
+	ImageView fileView;
+	@BindView(R.id.back_arrow)
+	MaterialIconView backArrow;
+	@BindView(R.id.fileName)
+	TextView fileName;
+	@BindView(R.id.file_options)
+	MaterialIconView fileOptions;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.content_file_view);
 		ButterKnife.bind(this);
+	}
+
+	@OnClick(R.id.file_options)
+	void onFileOptionClicked(View view) {
+		BottomSheetFileDetailsFragment.newInstance("fileId").show(getSupportFragmentManager(), BottomSheetFileDetailsFragment.class.getSimpleName());
+
+	}
+
+	@OnClick(R.id.back_arrow)
+	void onBackArrowClicked(View view) {
+		finish();
 	}
 }
